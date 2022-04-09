1ffe16a751ae78704ca62f56b770a0c5612e74dc
Rodion <rivannikov2001@gmail.com>
2022-04-09 23:04:10 +0300

    Implementing new account creating fixture, fixing tests logic

--- testing/code/test_cases/base_case.py
+++ testing/code/test_cases/base_case.py
@@ -50,6 +50,23 @@ class BaseCase:
         self.api_client.get_delete_user(user_data[0])
 
     @pytest.fixture(scope='function')
+    def new_user_to_db(self, user_data):
+        self.mysql_client.session.commit()
+        new_user = AppUsers(username=user_data[0],
+                            password=user_data[1],
+                            email=user_data[2],
+                            access=1
+                            )
+        self.mysql_client.session.add(new_user)
+        self.mysql_client.session.commit()
+        yield
+        self.mysql_client.session.query(AppUsers).filter(AppUsers.username == user_data[0],
+                                                         AppUsers.password == user_data[1],
+                                                         AppUsers.email == user_data[2]
+                                                         ).delete()
+        self.mysql_client.session.commit()
+
+    @pytest.fixture(scope='function')
     def user_data(self):
         return self.random_ascii(5, 16), self.random_ascii(1, 255), self.fake.email()
 
--- testing/code/test_cases/test_docker.py
+++ testing/code/test_cases/test_docker.py
@@ -9,7 +9,7 @@ class TestApi(BaseCase):
         assert response.status_code == 201, 'Status code must be 201'
         assert self.check_user_pass_email(*self.user_data)
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_add_existing_user(self):
         response = self.api_client.post_add_user(*self.user_data)
         assert response.status_code == 304
@@ -18,7 +18,7 @@ class TestApi(BaseCase):
         response = self.api_client.get_delete_user(self.username)
         assert response.status_code == 404
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_del_user(self):
         response = self.api_client.get_delete_user(self.username)
         assert response.status_code == 204 and not self.check_user(self.username)
@@ -27,30 +27,30 @@ class TestApi(BaseCase):
         response = self.api_client.get_status()
         assert response.status_code == 200
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_block_user(self):
         response = self.api_client.get_block_user(self.username)
         assert response.status_code == 200 and self.check_user_access(self.username, 0)
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_unblock_user(self):
         self.api_client.get_block_user(self.username)
         response = self.api_client.get_unblock_user(self.username)
         assert response.status_code == 200 and self.check_user_access(self.username, 1)
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_negative_add_user_with_existing_email(self):
         new_username = self.random_ascii(5, 16)
         response = self.api_client.post_add_user(new_username, *self.user_data[1:3])
         assert response.status_code != 500 and not response.ok and \
-               self.check_user_pass_email(*self.user_data)
+               not self.check_user_pass_email(new_username, *self.user_data[1:3])
 
-    @pytest.mark.usefixtures('new_user')
+    @pytest.mark.usefixtures('new_user_to_db')
     def test_negative_add_user_with_existing_username(self):
         new_email = self.fake.email()
         response = self.api_client.post_add_user(*self.user_data[0:2], new_email)
         assert response.status_code != 500 and not response.ok and \
-               self.check_user_pass_email(*self.user_data)
+               not self.check_user_pass_email(*self.user_data[0:2], new_email)
 
 
 class TestApiFieldValid(BaseCase):
@@ -64,5 +64,5 @@ class TestApiFieldValid(BaseCase):
     @pytest.mark.parametrize('email', BaseCase.read_file('emails_invalid.txt'))
     def test_fields_validation_negative(self, email):
         response = self.api_client.post_add_user(*self.user_data[0:2], email)
-        assert not self.check_user_pass_email(*self.user_data[0:2], email) and \
-               response.status_code != 500 and not response.ok
+        assert not response.status_code != 500 and not response.ok and \
+               not self.check_user_pass_email(*self.user_data[0:2], email)
