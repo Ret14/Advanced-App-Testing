097e54600743f461c4011b4d5a9f9ad74ef89e18
Rodion <rivannikov2001@gmail.com>
2022-04-17 18:11:28 +0300

    Fixing test dirs duplication error

--- testing/code/conftest.py
+++ testing/code/conftest.py
@@ -42,16 +42,8 @@ def pytest_unconfigure(config):
 
 @pytest.fixture(scope='function')
 def temp_dir(request):
-    components = request.node.nodeid.split("::")
-    # filename = components[0]
-    # test_class = components[1] if len(components) == 3 else None
-    test_func_with_params = components[-1]
-    test_func = test_func_with_params.split('[')[0]
-    if '[' in test_func_with_params:
-        test_params = test_func_with_params.split('[')[1][:-1].split('-')
-        test_func = f"{test_func}['{test_params[0]}']"
-
-    test_dir = os.path.join(request.config.base_temp_dir, test_func)
+    test_dir = os.path.join(request.config.base_temp_dir,
+                            request._pyfuncitem.node.originalname.replace('/', '_').replace(':', '_'))
     os.makedirs(test_dir)
     return test_dir
 
